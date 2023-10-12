881031431552758af777390fce825e8e2b0d7868
Vennela <96403605+Vennelareddycs@users.noreply.github.com>
2023-10-13 00:12:17 +0530

    Updated simple blog

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+
+def get_date(post):
+    return post['date']
+
+# Create your views here.
+def starting_page(request):
+    latest_posts=Post.objects.all().order_by("-date")[:3]
+    # sorted_posts=sorted(all_posts,key= get_date)
+    # latest_posts=sorted_posts[-3:]
+    return render(request, "blog/index.html",{
+        "posts": latest_posts,
+        
+    })
+
+def posts(request):
+    all_posts=Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html",
+    {
+        "all_posts":all_posts
+    })
+
+def post_detail(request, slug):
+    identified_post=get_object_or_404(Post, slug=slug)
+    #identified_post=Post.objects.get(slug=slug)
+    #identified_post=next(post for post in all_posts if post['slug']== slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
+    })
+
+'''
+#dummy data
 all_posts=[
     {
         "slug":"hike-in-the-mountains",
@@ -58,25 +92,4 @@ all_posts=[
     }
 ]
 
-def get_date(post):
-    return post['date']
-
-# Create your views here.
-def starting_page(request):
-    sorted_posts=sorted(all_posts,key= get_date)
-    latest_posts=sorted_posts[-3:]
-    return render(request, "blog/index.html",{
-        "posts": latest_posts
-    })
-
-def posts(request):
-    return render(request, "blog/all-posts.html",
-    {
-        "all_posts":all_posts
-    })
-
-def post_detail(request, slug):
-    identified_post=next(post for post in all_posts if post['slug']== slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post
-    })
+'''
